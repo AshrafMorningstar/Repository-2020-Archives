a5e97cccff35ec7bc8dc70f6fa9f0d69e540700d
Ashraf Morningstar <ashrafmorningstar@gmail.com>
2026-01-03 17:08:03 +0530

    chore(copyright): update headers and license information © 2026

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/*
+ Copyright (c) 2026 Ashraf Morningstar
+ These are personal recreations of existing projects, developed by Ashraf Morningstar
+ for learning and skill development.
+ Original project concepts remain the intellectual property of their respective creators.
+ Repository: https://github.com/AshrafMorningstar
+*/
+
 document.addEventListener('DOMContentLoaded', () => {
     // Console easter egg
     console.log("%c ARCTIC CODE VAULT ", "background: #58a6ff; color: #040912; font-weight: bold; padding: 5px; border-radius: 3px;");
